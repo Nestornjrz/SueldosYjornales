cafea72e3f6ce085ce880ed0582f21473e201b17
Nestornjrz <nestornjrz@hotmail.com>
2015-06-15 15:55:47 -0400

    Se crea el controlador mvc5 que servira el html de los Empleados

---
+++ SueldosYjornales/Controllers/EmpleadosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SueldosYjornales.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        // GET: Empleados
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
