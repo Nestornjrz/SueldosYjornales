b04843dfdea28fa962458b84b38a7bc77b4ff0ee
Nestornjrz <nestornjrz@hotmail.com>
2015-06-16 09:40:11 -0400

    Preparando para desarrollar los formularios de carga de los padrones, es decir las tablas uno.

---
+++ SueldosYjornales/Controllers/PadronesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SueldosYjornales.Controllers
+{
+    public class PadronesController : Controller
+    {
+        // GET: Padrones
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
