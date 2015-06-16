9561693996f7be14be2157f70fa18eac3cfb6db6
Nestornjrz <nestornjrz@hotmail.com>
2015-06-16 10:25:30 -0400

    Se preparo el front end de la carga de estancias, el formulario

---
+++ SueldosYjornales/Scripts/Padrones/Index/Controllers/empresasCtrl.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('sueldosYjornalesApp')
+        .controller('empresasCtrl', empresasCtrl);
+
+    empresasCtrl.$inject = ['$location']; 
+
+    function empresasCtrl($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.empresa = {};
+        vm.nuevoParaCargar = function () {
+            vm.empresa = {};
+        };
+
+        vm.guardar = function () {
+            proveedoresResource.empresas.save(vm.empresa)
+           .$promise.then(
+               function (mensaje) {
+                   if (!mensaje.error) {
+                       vm.empresa = mensaje.objetoDto;
+                       vm.mensajeDelServidor = mensaje.mensajeDelProceso;
+                       //$rootScope.$broadcast('actualizarListadoEmpresas', {});
+                   } else {
+                       vm.mensajeDelServidor = mensaje.mensajeDelProceso;
+                   }
+               },
+             function (mensaje) {
+                 vm.mensajeDelServidor = mensaje.data.mensajeDelProceso;
+             }
+           );
+        }
+    }
+})();
---
+++ SueldosYjornales/Scripts/Padrones/Index/Controllers/menuCtrl.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('sueldosYjornalesApp')
+        .controller('menuCtrl', menuCtrl);
+
+    menuCtrl.$inject = ['$location']; 
+
+    function menuCtrl($location) {      
+        var vm = this;
+        vm.menu = {};
+        vm.menu.introduccion = true;
+
+        vm.empresasFn = function () {
+            ocultar();
+            vm.menu.empresas.class = "active";
+            vm.menu.empresas.mostrar = true;
+        }
+      
+        //////
+        function ocultar() {
+            vm.menu.introduccion = false;
+            vm.menu.empresas = {};           
+        }
+    }
+})();
