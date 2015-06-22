a074ce5afad75403e727990ca0f9445a7fb050c7
Nestornjrz <nestornjrz@hotmail.com>
2015-06-22 11:35:43 -0400

    Implementacion de Managers de Nacionalidades

--- SYJ.Domain.Managers/NacionalidadesManagers.cs
+++ SYJ.Domain.Managers/NacionalidadesManagers.cs
@@ -1,4 +1,6 @@
 ﻿using SYJ.Application.Dto;
+using SYJ.Domain.Db;
+using SYJ.Domain.Managers.Util;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,15 +11,88 @@ namespace SYJ.Domain.Managers {
     public class NacionalidadesManagers {
 
         public List<NacionalidadeDto> ListadoNacionalidades() {
-            throw new NotImplementedException();
+            using (var context = new SueldosJornalesEntities()) {
+                var listado = context.Nacionalidades
+                    .Select(n => new NacionalidadeDto() {
+                        NacionalidadID = n.NacionalidadID,
+                        NombreNacionalidad = n.NombreNacionalidad,
+                        Abreviatura = n.Abreviatura
+                    }).ToList();
+                return listado;
+            }
         }
 
         public MensajeDto CargarNacionalidad(NacionalidadeDto nDto) {
-            throw new NotImplementedException();
+            using (var context = new SueldosJornalesEntities()) {
+                if (context.Nacionalidades.Where(n => n.NacionalidadID == nDto.NacionalidadID).Count() > 0) {
+                    return EditarNacionalidad(nDto);
+                }
+                MensajeDto mensajeDto = null;
+                var nacionalidadeDb = new Nacionalidade();
+                nacionalidadeDb.NacionalidadID = nDto.NacionalidadID;
+                nacionalidadeDb.NombreNacionalidad = nDto.NombreNacionalidad;
+                nacionalidadeDb.Abreviatura = nDto.Abreviatura;
+
+                context.Nacionalidades.Add(nacionalidadeDb);
+
+                mensajeDto = AgregarModificar.Hacer(context, mensajeDto);
+                if (mensajeDto != null) { return mensajeDto; }
+
+                nDto.NacionalidadID = nacionalidadeDb.NacionalidadID;
+
+                return new MensajeDto() {
+                    Error = false,
+                    MensajeDelProceso = "Se cargo la nacionalidad : " + nDto.NacionalidadID,
+                    ObjetoDto = nDto
+                };
+            }
+        }
+
+        private MensajeDto EditarNacionalidad(NacionalidadeDto nDto) {
+            using (var context = new SueldosJornalesEntities()) {
+                MensajeDto mensajeDto = null;
+                var nacionalidadeDb = context.Nacionalidades
+                    .Where(n => n.NacionalidadID == nDto.NacionalidadID)
+                    .First();
+                nacionalidadeDb.NombreNacionalidad = nDto.NombreNacionalidad;
+                nacionalidadeDb.Abreviatura = nDto.Abreviatura;
+
+                context.Entry(nacionalidadeDb).State = System.Data.Entity.EntityState.Modified;
+                mensajeDto = AgregarModificar.Hacer(context, mensajeDto);
+                if (mensajeDto != null) { return mensajeDto; }
+
+                return new MensajeDto() {
+                    Error = false,
+                    MensajeDelProceso = "Se Edito la nacionalidad : " + nDto.NacionalidadID,
+                    ObjetoDto = nDto
+                };
+            }
         }
 
         public MensajeDto EliminarNacionalidad(int id) {
-            throw new NotImplementedException();
+            using (var context = new SueldosJornalesEntities()) {
+                MensajeDto mensajeDto = null;
+                var nacionalidadeDb = context.Nacionalidades
+                   .Where(n => n.NacionalidadID == id)
+                   .First();
+
+                if (nacionalidadeDb == null) {
+                    return new MensajeDto() {
+                        Error = true,
+                        MensajeDelProceso = "La nacionalidad ID : " + id + " no existe en la base de datos"
+                    };
+                }
+
+                context.Nacionalidades.Remove(nacionalidadeDb);
+
+                mensajeDto = AgregarModificar.Hacer(context, mensajeDto);
+                if (mensajeDto != null) { return mensajeDto; }
+
+                return new MensajeDto() {
+                    Error = false,
+                    MensajeDelProceso = "Se elimino la nacionalidad : " + id
+                };
+            }
         }
     }
 }
--- SueldosYjornales/Scripts/Padrones/Index/Controllers/nacionalidades/nacionalidadesCtrl.js
+++ SueldosYjornales/Scripts/Padrones/Index/Controllers/nacionalidades/nacionalidadesCtrl.js
@@ -10,13 +10,17 @@
     function nacionalidadesCtrl($rootScope, sYjResource) {
         /* jshint validthis:true */
         var vm = this;
+        vm.nacionalidade = {};
+        vm.nacionalidade.cargarSn = true;
+
         vm.guardar = function () {
             sYjResource.nacionalidades.save(vm.nacionalidade)
           .$promise.then(
               function (mensaje) {
                   if (!mensaje.error) {
                       vm.nacionalidade = mensaje.objetoDto;
-                      vm.mensajeDelServidor = mensaje.mensajeDelProceso;                     
+                      vm.mensajeDelServidor = mensaje.mensajeDelProceso;
+                      vm.nacionalidade.cargarSn = false;
                   } else {
                       vm.mensajeDelServidor = mensaje.mensajeDelProceso;
                   }
@@ -28,6 +32,7 @@
         }
         vm.nuevoParaCargar = function () {
             vm.nacionalidade = {};
+            vm.nacionalidade.cargarSn = true;
         }
     }
 })();
