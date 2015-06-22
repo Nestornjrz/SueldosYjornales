9d67cda5d9f01778ea3a9afa52684d2ccfde0e02
Nestornjrz <nestornjrz@hotmail.com>
2015-06-22 10:37:44 -0400

    Preparacion del formulario y la parte del controlador con los aspectos basicos de la carga.

--- SueldosYjornales/Scripts/Padrones/Index/Controllers/nacionalidades/nacionalidadesCtrl.js
+++ SueldosYjornales/Scripts/Padrones/Index/Controllers/nacionalidades/nacionalidadesCtrl.js
@@ -2,14 +2,32 @@
     'use strict';
 
     angular
-        .module('app')
-        .controller('sueldosYjornalesApp', nacionalidadesCtrl);
+        .module('sueldosYjornalesApp')
+        .controller('nacionalidadesCtrl', nacionalidadesCtrl);
 
     nacionalidadesCtrl.$inject = ['$rootScope', 'sYjResource'];
 
     function nacionalidadesCtrl($rootScope, sYjResource) {
         /* jshint validthis:true */
         var vm = this;
-        
+        vm.guardar = function () {
+            sYjResource.nacionalidades.save(vm.nacionalidade)
+          .$promise.then(
+              function (mensaje) {
+                  if (!mensaje.error) {
+                      vm.nacionalidade = mensaje.objetoDto;
+                      vm.mensajeDelServidor = mensaje.mensajeDelProceso;                     
+                  } else {
+                      vm.mensajeDelServidor = mensaje.mensajeDelProceso;
+                  }
+              },
+            function (mensaje) {
+                vm.mensajeDelServidor = mensaje.data.mensajeDelProceso;
+            }
+          );
+        }
+        vm.nuevoParaCargar = function () {
+            vm.nacionalidade = {};
+        }
     }
 })();
