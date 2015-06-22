c1911c57b954b9b265bd48d130618a95824f54e0
Nestornjrz <nestornjrz@hotmail.com>
2015-06-22 16:57:22 -0400

    Implementacion del modal para elimnar registros en cargos del "Padron"

--- SueldosYjornales/Scripts/Padrones/Index/Controllers/cargos/listadoCargosCtrl.js
+++ SueldosYjornales/Scripts/Padrones/Index/Controllers/cargos/listadoCargosCtrl.js
@@ -12,8 +12,34 @@
         var vm = this;
         vm.cargos = sYjResource.cargos.query();
 
-        vm.eliminar = function () {
+        vm.eliminar = function (cargo) {
+            var modalInstance = $modal.open({
+                templateUrl: 'ModalEliminacionCargo.html',
+                controller: function ($scope, $modalInstance) {
+                    $scope.cargo = cargo;
+                    $scope.objeto = {};
+                    $scope.objeto.id = cargo.cargoID;
+                    $scope.objeto.mensaje = "Se eliminara el cargo numero ";
+                    $scope.ok = function () {
+                        sYjResource.cargos.delete({ id: cargo.cargoID },
+                              function (respuesta) {
+                                  $scope.respuesta = respuesta;
+                                  vm.cargos = sYjResource.cargos.query();
+                              });
+                        //$rootScope.$broadcast('actualizarTodos', {});
+                    };
+                    $scope.cancel = function () {
+                        $modalInstance.dismiss('cancel');
+                    };
+                },
+                size: 'sm'
+            });
+            modalInstance.result.then(function (selectedItem) {
 
+            }, function () {
+                //$log.info('Modal dismissed at: ' + new Date());
+                vm.cargos = sYjResource.cargos.query();
+            });
         }
         vm.actualizar = function (cargo) {
             $rootScope.$broadcast('actualizarCargo', cargo);
