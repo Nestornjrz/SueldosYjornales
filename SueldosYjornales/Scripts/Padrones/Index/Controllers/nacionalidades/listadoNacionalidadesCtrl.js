edd6353e60f3f0920f3c7365c5048ad8064edbad
Nestornjrz <nestornjrz@hotmail.com>
2015-06-22 11:57:53 -0400

    Preparacion del listado de nacionalidades en el "padron" en el FronEnd

---
+++ SueldosYjornales/Scripts/Padrones/Index/Controllers/nacionalidades/listadoNacionalidadesCtrl.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('sueldosYjornalesApp')
+        .controller('listadoNacionalidadesCtrl', listadoNacionalidadesCtrl);
+
+    listadoNacionalidadesCtrl.$inject = ['$rootScope', '$modal', 'sYjResource'];
+
+    function listadoNacionalidadesCtrl($rootScope, $modal, sYjResource) {
+        /* jshint validthis:true */
+        var vm = this;
+        
+    }
+})();
