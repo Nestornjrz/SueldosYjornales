63fa5e7f9ba91d95cdd7747b3df382466afea73a
Nestornjrz <nestornjrz@hotmail.com>
2015-06-23 16:41:32 -0400

    Preparacion del Api de Usuarios en el "Padron"

---
+++ SYJ.Domain.Managers/UsuariosManagers.cs
@@ -0,0 +1,23 @@
+﻿using SYJ.Application.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SYJ.Domain.Managers {
+    public class UsuariosManagers {
+
+        public List<UsuarioDto> ListadoUsuarios() {
+            throw new NotImplementedException();
+        }
+
+        public MensajeDto CargarUsuarios(UsuarioDto uDto) {
+            throw new NotImplementedException();
+        }
+
+        public MensajeDto EliminarUsuarios(int id) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SueldosYjornales/Controllers/Api/UsuariosController.cs
@@ -0,0 +1,42 @@
+﻿using SYJ.Application.Dto;
+using SYJ.Domain.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SueldosYjornales.Controllers.Api {
+    public class UsuariosController : ApiController {
+        // GET: api/Usuarios
+        public HttpResponseMessage Get() {
+            UsuariosManagers um = new UsuariosManagers();
+            List<UsuarioDto> listado = um.ListadoUsuarios();
+            return Request.CreateResponse<List<UsuarioDto>>(HttpStatusCode.OK, listado);
+        }
+
+        // GET: api/Usuarios/5
+        public string Get(int id) {
+            return "value";
+        }
+
+        // POST: api/Usuarios
+        public HttpResponseMessage Post(UsuarioDto uDto) {
+            UsuariosManagers um = new UsuariosManagers();
+            MensajeDto mensaje = um.CargarUsuarios(uDto);
+            return Request.CreateResponse(HttpStatusCode.Created, mensaje);
+        }
+
+        // PUT: api/Usuarios/5
+        public void Put(int id, [FromBody]string value) {
+        }
+
+        // DELETE: api/Usuarios/5
+        public HttpResponseMessage Delete(int id) {
+            UsuariosManagers um = new UsuariosManagers();
+            MensajeDto mensaje = um.EliminarUsuarios(id);
+            return Request.CreateResponse(HttpStatusCode.Created, mensaje);
+        }
+    }
+}
