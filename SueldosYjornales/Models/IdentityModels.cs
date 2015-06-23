8d721d35541c7b187107aba28c7cde5f050f34f7
Nestornjrz <nestornjrz@hotmail.com>
2015-06-23 11:44:40 -0400

    Cambio del nombre del conectionString para que apunte al contex de la capa Domain.Db

--- SueldosYjornales/Models/IdentityModels.cs
+++ SueldosYjornales/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace SueldosYjornales.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("SueldosJornalesEntities", throwIfV1Schema: false)
         {
         }
 
