5d96f0b37776c730cc9cbd9eeb40b85d48a5b418
Nestornjrz <nestornjrz@hotmail.com>
2015-06-23 11:33:50 -0400

    Preparacion del formulario de carga de usuarios y la parte basica del controladorJS

---
+++ SueldosYjornales/Scripts/Padrones/Index/Controllers/usuarios/usuariosCtrl.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('sueldosYjornalesApp')
+        .controller('usuariosCtrl', usuariosCtrl);
+
+    usuariosCtrl.$inject = ['$rootScope', 'sYjResource'];
+
+    function usuariosCtrl($rootScope, sYjResource) {
+        /* jshint validthis:true */
+        var vm = this;
+        
+    }
+})();
