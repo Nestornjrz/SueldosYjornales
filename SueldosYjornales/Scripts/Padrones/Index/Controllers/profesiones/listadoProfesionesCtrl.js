af4dddf2be268811a0e63e1750aebb08d6d87c63
Nestornjrz <nestornjrz@hotmail.com>
2015-06-23 10:50:09 -0400

    Implentacion de la eliminacion de de profesion en el padron

--- SueldosYjornales/Scripts/Padrones/Index/Controllers/profesiones/listadoProfesionesCtrl.js
+++ SueldosYjornales/Scripts/Padrones/Index/Controllers/profesiones/listadoProfesionesCtrl.js
@@ -15,7 +15,36 @@
         vm.actualizar = function (profesione) {
             $rootScope.$broadcast('actualizarProfesione',profesione);
         }
+        vm.eliminar = function (profesione) {
+            var modalInstance = $modal.open({
+                templateUrl: 'ModalEliminacionProfesion.html',
+                controller: function ($scope, $modalInstance) {
+                    $scope.profesione = profesione;
+                    $scope.objeto = {};
+                    $scope.objeto.id = profesione.profesionID;
+                    $scope.objeto.mensaje = "Se eliminara la profesion numero ";
+                    $scope.ok = function () {
+                        sYjResource.profesiones.delete({ id: profesione.profesionID },
+                              function (respuesta) {
+                                  $scope.respuesta = respuesta;
+                                  vm.profesiones = sYjResource.profesiones.query();
+                              });
 
+                        //$rootScope.$broadcast('actualizarTodos', {});
+                    };
+                    $scope.cancel = function () {
+                        $modalInstance.dismiss('cancel');
+                    };
+                },
+                size: 'sm'
+            });
+            modalInstance.result.then(function (selectedItem) {
+
+            }, function () {
+                //$log.info('Modal dismissed at: ' + new Date());
+                vm.profesiones = sYjResource.profesiones.query();
+            });
+        }
 
         //Capturando eventos de usuario
         $rootScope.$on('actualizarListadoProfesiones', function (event, objValRecibido) {
