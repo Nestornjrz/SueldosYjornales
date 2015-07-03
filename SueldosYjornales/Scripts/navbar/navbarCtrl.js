f1755b8851dc2494a13fb497ec7f32a864cda9f1
Nestornjrz <nestornjrz@hotmail.com>
2015-07-03 15:41:21 -0400

    Coloco la sucursal en donde se esta trabajando en el NavBar, pues es importante que el usuario sepa en que sucursal esta trabajando.

---
+++ SueldosYjornales/Scripts/navbar/navbarCtrl.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('sueldosYjornalesApp')
+        .controller('navbarCtrl', navbarCtrl);
+
+    navbarCtrl.$inject = ['$scope', '$rootScope', 'sYjResource'];
+
+    function navbarCtrl($scope, $rootScope, sYjResource) {
+        sYjResource.ubicacionSucUsuarios.get(function (respuesta) {
+            $scope.ubicacionSucUsuario = respuesta.objetoDto;
+        });
+    }
+})();
