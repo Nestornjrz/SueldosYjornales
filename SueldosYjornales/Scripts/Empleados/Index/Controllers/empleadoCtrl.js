56d97f380bb1a382d9543aedd5bdcb01eae25523
Nestornjrz <nestornjrz@hotmail.com>
2015-07-03 17:26:49 -0400

    Se prepara el formulario de carga de Empleados

---
+++ SueldosYjornales/Scripts/Empleados/Index/Controllers/empleadoCtrl.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('sueldosYjornalesApp')
+        .controller('empleadoCtrl', empleadoCtrl);
+
+    empleadoCtrl.$inject = ['$scope','$rootScope', '$modal', 'sYjResource'];
+
+    function empleadoCtrl($scope, $rootScope, $modal, sYjResource) {
+        $scope.sexos = [
+            { id: 1, sexo: "Masculino" },
+            { id: 2, sexo: "Femenino" }
+        ];
+    }
+})();
