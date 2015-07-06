ba21003d5feb4d6d0982f72ce7189da1301d6397
Nestornjrz <nestornjrz@hotmail.com>
2015-07-06 13:48:53 -0400

    Implementacion de la eliminacion del registro de empleados

--- SueldosYjornales/Scripts/Empleados/Index/Controllers/listadoEmpleadosCtrl.js
+++ SueldosYjornales/Scripts/Empleados/Index/Controllers/listadoEmpleadosCtrl.js
@@ -13,5 +13,38 @@
         $scope.actualizar = function (empleado) {
             $rootScope.$broadcast('actualizarEmpleado', empleado);
         }
+
+        $scope.eliminar = function (empleado) {
+            var modalInstance = $modal.open({
+                templateUrl: 'ModalEliminacionEmpleado.html',
+                controller: function ($scope, $modalInstance) {
+                    $scope.empleado = empleado;
+                    $scope.objeto = {};
+                    $scope.objeto.id = empleado.empleadoID;
+                    $scope.objeto.mensaje = "Se eliminara el empleado numero ";
+                    $scope.ok = function () {
+                        sYjResource.empleados.delete({ id: empleado.empleadoID },
+                              function (respuesta) {
+                                  $scope.respuesta = respuesta;
+                                  $scope.empleados = sYjResource.empleados.query();
+                              });
+                        //$rootScope.$broadcast('actualizarTodos', {});
+                    };
+                    $scope.cancel = function () {
+                        $modalInstance.dismiss('cancel');
+                    };
+                },
+                size: 'sm'
+            });
+            modalInstance.result.then(function (selectedItem) {
+
+            }, function () {
+                //$log.info('Modal dismissed at: ' + new Date());
+                $scope.empleados = sYjResource.empleados.query();
+            });
+        }
+        $rootScope.$on('actualizarListadoEmpleados', function (event,objValRecibido) {
+            $scope.empleados = sYjResource.empleados.query();
+        });
     }
 })();
