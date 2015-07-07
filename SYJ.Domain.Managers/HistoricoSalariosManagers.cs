414a88b23bb695671dd4dd3efa5a36fea816a018
Nestornjrz <nestornjrz@hotmail.com>
2015-07-07 15:47:21 -0400

    Preparo el Web Api de Salarios.

---
+++ SYJ.Domain.Managers/HistoricoSalariosManagers.cs
@@ -0,0 +1,26 @@
+﻿using SYJ.Application.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SYJ.Domain.Managers {
+    public class HistoricoSalariosManagers {
+        public List<HistoricoSalarioDto> ListadoHistoricoSalarios() {
+            throw new NotImplementedException();
+        }
+
+        public MensajeDto CargarHistoricoSalario(HistoricoSalarioDto hsDto) {
+            throw new NotImplementedException();
+        }
+
+        public MensajeDto EliminarHistoricoSalario(int id) {
+            throw new NotImplementedException();
+        }
+
+        public List<HistoricoSalarioDto> ListadoHistoricoSalarios(long empleadoID) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SueldosYjornales/Controllers/Api/HistoricoSalariosController.cs
@@ -0,0 +1,57 @@
+﻿using SYJ.Application.Dto;
+using SYJ.Domain.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SueldosYjornales.Controllers.Api
+{
+    public class HistoricoSalariosController : ApiController
+    {
+        // GET: api/HistoricoSalarios
+        public HttpResponseMessage Get()
+        {
+            HistoricoSalariosManagers hsm = new HistoricoSalariosManagers();
+            List<HistoricoSalarioDto> listado = hsm.ListadoHistoricoSalarios();
+            return Request.CreateResponse<List<HistoricoSalarioDto>>(HttpStatusCode.OK, listado);
+        }
+
+        [HttpGet]
+        [Route("api/HistoricoSalarios/ByEmpleadoID")]
+        public HttpResponseMessage GetByEmpleadosID(long empleadoID) {
+            HistoricoSalariosManagers hsm = new HistoricoSalariosManagers();
+            List<HistoricoSalarioDto> listado = hsm.ListadoHistoricoSalarios(empleadoID);
+            return Request.CreateResponse<List<HistoricoSalarioDto>>(HttpStatusCode.OK, listado);
+        }
+
+        // GET: api/HistoricoSalarios/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/HistoricoSalarios
+        public HttpResponseMessage Post(HistoricoSalarioDto hsDto)
+        {
+            HistoricoSalariosManagers hsm = new HistoricoSalariosManagers();
+            MensajeDto mensaje = hsm.CargarHistoricoSalario(hsDto);
+            return Request.CreateResponse(HttpStatusCode.Created, mensaje);
+        }
+
+        // PUT: api/HistoricoSalarios/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/HistoricoSalarios/5
+        public HttpResponseMessage Delete(int id)
+        {
+            HistoricoSalariosManagers hsm = new HistoricoSalariosManagers();
+            MensajeDto mensaje = hsm.EliminarHistoricoSalario(id);
+            return Request.CreateResponse(HttpStatusCode.Created, mensaje);
+        }
+    }
+}
