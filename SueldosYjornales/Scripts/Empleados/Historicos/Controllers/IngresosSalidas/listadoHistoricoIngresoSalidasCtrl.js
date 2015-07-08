2c3fd9679d33d31cbd6b07849066e4cf24241870
Nestornjrz <nestornjrz@hotmail.com>
2015-07-08 13:19:52 -0400

    Implementacion de la eliminacion de registros de ingresos y salidas

--- SueldosYjornales/Scripts/Empleados/Historicos/Controllers/IngresosSalidas/listadoHistoricoIngresoSalidasCtrl.js
+++ SueldosYjornales/Scripts/Empleados/Historicos/Controllers/IngresosSalidas/listadoHistoricoIngresoSalidasCtrl.js
@@ -5,14 +5,47 @@
         .module('sueldosYjornalesApp')
         .controller('listadoHistoricoIngresoSalidasCtrl', listadoHistoricoIngresoSalidasCtrl);
 
-    listadoHistoricoIngresoSalidasCtrl.$inject = ['$rootScope', 'sYjResource'];
+    listadoHistoricoIngresoSalidasCtrl.$inject = ['$rootScope', '$modal', 'sYjResource'];
 
-    function listadoHistoricoIngresoSalidasCtrl($rootScope, sYjResource) {
+    function listadoHistoricoIngresoSalidasCtrl($rootScope, $modal, sYjResource) {
         /* jshint validthis:true */
         var vm = this;
         
         vm.actualizar = function (historicoIngresoSalida) {
             $rootScope.$broadcast('actualizarHistoricoIngresoSalida', historicoIngresoSalida);
+        }
+
+        vm.eliminar = function (historicoIngresoSalida) {
+            var modalInstance = $modal.open({
+                templateUrl: 'ModalEliminacionHistoricoIngresoSalida.html',
+                controller: function ($scope, $modalInstance) {
+                    $scope.historicoIngresoSalida = historicoIngresoSalida;
+                    $scope.objeto = {};
+                    $scope.objeto.id = historicoIngresoSalida.historicoIngresoSalidaID;
+                    $scope.objeto.mensaje = "Se eliminara el Historico salario numero ";
+                    $scope.ok = function () {
+                        sYjResource.historicoIngresoSalidas
+                            .delete({ id: historicoIngresoSalida.historicoIngresoSalidaID },
+                              function (respuesta) {
+                                  $scope.respuesta = respuesta;
+                                  vm.historicoIngresoSalidas = sYjResource
+                .historicoIngresoSalidasByEmpleadoID.query({ 'empleadoID': vm.empleadoID });
+                              });
+                        //$rootScope.$broadcast('actualizarTodos', {});
+                    };
+                    $scope.cancel = function () {
+                        $modalInstance.dismiss('cancel');
+                    };
+                },
+                size: 'sm'
+            });
+            modalInstance.result.then(function (selectedItem) {
+
+            }, function () {
+                //$log.info('Modal dismissed at: ' + new Date());
+                vm.historicoIngresoSalidas = sYjResource
+                .historicoIngresoSalidasByEmpleadoID.query({ 'empleadoID': vm.empleadoID });
+            });
         }
 
         //Eventos
