eb5e0ada18a1f53bfa7f4e34b337c048b76c926e
Nestornjrz <nestornjrz@hotmail.com>
2015-07-08 09:45:43 -0400

    Prepare el formulario de carga de historico de ingresos y salidas

---
+++ SueldosYjornales/Scripts/Empleados/Historicos/Controllers/IngresosSalidas/historicoIngresoSalidasCtrl.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('sueldosYjornalesApp')
+        .controller('historicoIngresoSalidasCtrl', historicoIngresoSalidasCtrl);
+
+    historicoIngresoSalidasCtrl.$inject = ['$rootScope', 'sYjResource'];
+
+    function historicoIngresoSalidasCtrl($rootScope, sYjResource) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.historicoIngresoSalida = {};
+       
+        //Eventos
+        $rootScope.$on('empleadoID', function (event, objValRecibido) {
+            vm.historicoIngresoSalida.empleadoID = objValRecibido;
+            vm.empleadoID = objValRecibido;
+        });
+    }
+})();
