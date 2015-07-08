25a8e41e4d934d3f27a7cc82d01908a1e2da4397
Nestornjrz <nestornjrz@hotmail.com>
2015-07-08 14:21:34 -0400

    Preparacion del formulario de carga de historico de horarios.

---
+++ SueldosYjornales/Scripts/Empleados/Historicos/Controllers/HistoricoHorarios/historicoHorariosCtrl.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('sueldosYjornalesApp')
+        .controller('historicoHorariosCtrl', historicoHorariosCtrl);
+
+    historicoHorariosCtrl.$inject = ['$rootScope', 'sYjResource'];
+
+    function historicoHorariosCtrl($rootScope, sYjResource) {
+        /* jshint validthis:true */
+        var vm = this;
+        
+        
+    }
+})();
