9dca60a7d31f2a981a07d12f12bb82ea07579ad3
Nestornjrz <nestornjrz@hotmail.com>
2015-07-27 14:04:29 -0400

    Preparacion del formulario para carga de historico de sucursales

---
+++ SueldosYjornales/Scripts/Empleados/Historicos/Controllers/HistoricoSucursales/historicoSucursalesCtrl.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('sueldosYjornalesApp')
+        .controller('historicoSucursalesCtrl', historicoSucursalesCtrl);
+
+    historicoSucursalesCtrl.$inject = ['$rootScope', '$modal', 'sYjResource'];
+
+    function historicoSucursalesCtrl($rootScope, $modal, sYjResource) {
+        /* jshint validthis:true */
+        var vm = this;
+      
+    }
+})();
