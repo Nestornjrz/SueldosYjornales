1292e3d3329dc0eb592752e3f3965f4c03b27a17
Nestornjrz <nestornjrz@hotmail.com>
2015-07-28 10:33:05 -0400

    Se crea el Web Api para Comisiones

---
+++ SYJ.Domain.Managers/ComisionesManagers.cs
@@ -0,0 +1,22 @@
+﻿using SYJ.Application.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SYJ.Domain.Managers {
+    public class ComisionesManagers {
+        public List<ComisioneDto> ListadoHistoricoHorarios(long empleadoID) {
+            throw new NotImplementedException();
+        }
+
+        public MensajeDto CargarComision(ComisioneDto cDto, Guid userID) {
+            throw new NotImplementedException();
+        }
+
+        public MensajeDto EliminarComision(int id) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SueldosYjornales/Controllers/Api/ComisionesController.cs
@@ -0,0 +1,56 @@
+﻿using SYJ.Application.Dto;
+using SYJ.Domain.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+
+namespace SueldosYjornales.Controllers.Api
+{
+    public class ComisionesController : ApiController
+    {
+        // GET: api/Comisiones
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet]
+        [Route("api/Comisiones/ByEmpleadoID")]
+        public HttpResponseMessage GetByEmpleadoID(long empleadoID) {
+            ComisionesManagers cm = new ComisionesManagers();
+            List<ComisioneDto> listado = cm.ListadoHistoricoHorarios(empleadoID);
+            return Request.CreateResponse<List<ComisioneDto>>(HttpStatusCode.OK, listado);
+        }
+
+        // GET: api/Comisiones/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Comisiones
+        public HttpResponseMessage Post(ComisioneDto cDto)
+        {
+            ComisionesManagers cm = new ComisionesManagers();
+            MensajeDto mensaje = cm.CargarComision(cDto, Guid.Parse(User.Identity.GetUserId()));
+            return Request.CreateResponse(HttpStatusCode.Created, mensaje);
+        }
+
+        // PUT: api/Comisiones/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Comisiones/5
+        public HttpResponseMessage Delete(int id)
+        {
+            ComisionesManagers cm = new ComisionesManagers();
+            MensajeDto mensaje = cm.EliminarComision(id);
+            return Request.CreateResponse(HttpStatusCode.Created, mensaje);
+        }
+    }
+}
