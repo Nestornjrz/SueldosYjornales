390aedd5242789714898a4c875c722e2239693d2
Nestornjrz <nestornjrz@hotmail.com>
2015-07-29 15:41:29 -0400

    Implemente la eliminacion de la comision en el Front-End.

--- SueldosYjornales/Scripts/Empleados/Details/Controllers/Comisiones/listadoComisionesCtrl.js
+++ SueldosYjornales/Scripts/Empleados/Details/Controllers/Comisiones/listadoComisionesCtrl.js
@@ -14,6 +14,40 @@
             $rootScope.$broadcast('actualizarComisione', comisione);
         }
 
+        vm.eliminar = function (comisione) {
+            var modalInstance = $modal.open({
+                templateUrl: 'ModalEliminacionComisione.html',
+                controller: function ($scope, $modalInstance) {
+                    $scope.comisione = comisione;
+                    $scope.objeto = {};
+                    $scope.objeto.id = comisione.comisionID;
+                    $scope.objeto.mensaje = "Se eliminara la comision numero ";
+                    $scope.ok = function () {
+                        sYjResource.comisiones
+                            .delete({ id: comisione.comisionID },
+                              function (respuesta) {
+                                  $scope.respuesta = respuesta;
+                                  vm.comisiones = sYjResource
+                                      .comisionesByEmpleadoID
+                                      .query({ 'empleadoID': vm.empleadoID });
+                              });
+                        //$rootScope.$broadcast('actualizarTodos', {});
+                    };
+                    $scope.cancel = function () {
+                        $modalInstance.dismiss('cancel');
+                    };
+                },
+                size: 'sm'
+            });
+            modalInstance.result.then(function (selectedItem) {
+
+            }, function () {
+                //$log.info('Modal dismissed at: ' + new Date());
+                vm.comisiones = sYjResource
+                .comisionesByEmpleadoID.query({ 'empleadoID': vm.empleadoID });
+            });
+        }
+
         //Eventos
         $rootScope.$on('empleadoID', function (event, objValRecibido) {
             vm.empleadoID = objValRecibido;
