6398584e761ed3ebf0b37900297454457933ac69
Nestornjrz <nestornjrz@hotmail.com>
2015-07-29 13:58:29 -0400

    Implemente el listado de comisiones, ademas ENTENDI el tema del UTC en el caso de las fechas.

--- SueldosYjornales/Scripts/Empleados/Details/Controllers/Comisiones/comisionesCtrl.js
+++ SueldosYjornales/Scripts/Empleados/Details/Controllers/Comisiones/comisionesCtrl.js
@@ -5,11 +5,49 @@
         .module('sueldosYjornalesApp')
         .controller('comisionesCtrl', comisionesCtrl);
 
-    comisionesCtrl.$inject = ['$location']; 
+    comisionesCtrl.$inject = ['$rootScope', 'sYjResource'];
 
-    function comisionesCtrl($location) {
+    function comisionesCtrl($rootScope, sYjResource) {
         /* jshint validthis:true */
         var vm = this;
-      
+        vm.comisione = {};
+        vm.nuevoParaCargar = function () {
+            vm.comisione = {};
+            vm.comisione.empleadoID = vm.empleadoID;
+        };
+
+        vm.guardar = function () {
+            sYjResource.comisiones.save(vm.comisione)
+          .$promise.then(
+              function (mensaje) {
+                  if (!mensaje.error) {
+                      vm.comisione = mensaje.objetoDto;
+                      vm.mensajeDelServidor = mensaje.mensajeDelProceso;
+                      vm.comisione.fechaComision = new Date(mensaje.objetoDto.fechaComision);
+                      $rootScope.$broadcast('actualizarListadoComisiones', {});
+                  } else {
+                      vm.mensajeDelServidor = mensaje.mensajeDelProceso;
+                  }
+              },
+            function (mensaje) {
+                vm.mensajeDelServidor = mensaje.data.mensajeDelProceso;
+            }
+          );
+        }
+
+        //Eventos
+        $rootScope.$on('empleadoID', function (event, objValRecibido) {
+            vm.comisione.empleadoID = objValRecibido;
+            vm.empleadoID = objValRecibido;
+        });
+
+        $rootScope.$on('actualizarComisione', function (event, objValRecibido) {
+            vm.comisione = objValRecibido;
+            vm.comisione.fechaComision = new Date(objValRecibido.fechaComision);
+            //vm.comisione.timezone = vm.comisione.fechaComision.getTimezoneOffset();
+            //vm.comisione.toLocaleDateString = vm.comisione.fechaComision.toLocaleDateString();
+            //vm.comisione.toUTCString = vm.comisione.fechaComision.toUTCString();
+            //vm.comisione.fechaComision = new Date(objValRecibido.fechaComision.slice(0, 10));
+        });
     }
 })();
---
+++ SueldosYjornales/Scripts/Empleados/Details/Controllers/Comisiones/listadoComisionesCtrl.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('sueldosYjornalesApp')
+        .controller('listadoComisionesCtrl', listadoComisionesCtrl);
+
+    listadoComisionesCtrl.$inject = ['$rootScope', '$modal', 'sYjResource'];
+
+    function listadoComisionesCtrl($rootScope, $modal, sYjResource) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.actualizar = function (comisione) {
+            $rootScope.$broadcast('actualizarComisione', comisione);
+        }
+
+        //Eventos
+        $rootScope.$on('empleadoID', function (event, objValRecibido) {
+            vm.empleadoID = objValRecibido;
+            vm.comisiones = sYjResource
+                .comisionesByEmpleadoID.query({ 'empleadoID': vm.empleadoID });
+        });
+
+        $rootScope.$on('actualizarListadoComisiones', function (event, objValRecibido) {
+            vm.comisiones = sYjResource
+               .comisionesByEmpleadoID.query({ 'empleadoID': vm.empleadoID });
+        });
+    }
+})();
