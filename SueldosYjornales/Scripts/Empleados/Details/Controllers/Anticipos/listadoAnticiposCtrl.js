10f60c25983b4ef9e01f7b96fa7025f0550d06c3
Nestornjrz <nestornjrz@hotmail.com>
2015-07-30 10:34:48 -0400

    Implementacion de la eliminacion de anticipos en el Front-End

--- SueldosYjornales/Scripts/Empleados/Details/Controllers/Anticipos/listadoAnticiposCtrl.js
+++ SueldosYjornales/Scripts/Empleados/Details/Controllers/Anticipos/listadoAnticiposCtrl.js
@@ -10,8 +10,42 @@
     function listadoAnticiposCtrl($rootScope, $modal, sYjResource) {
         /* jshint validthis:true */
         var vm = this;
-        vm.actualizar = function (comisione) {
-            $rootScope.$broadcast('actualizarAnticipo', comisione);
+        vm.actualizar = function (anticipo) {
+            $rootScope.$broadcast('actualizarAnticipo', anticipo);
+        }
+
+        vm.eliminar = function (anticipo) {
+            var modalInstance = $modal.open({
+                templateUrl: 'ModalEliminacionAnticipo.html',
+                controller: function ($scope, $modalInstance) {
+                    $scope.anticipo = anticipo;
+                    $scope.objeto = {};
+                    $scope.objeto.id = anticipo.anticipoID;
+                    $scope.objeto.mensaje = "Se eliminara el anticipo numero ";
+                    $scope.ok = function () {
+                        sYjResource.anticipos
+                            .delete({ id: anticipo.anticipoID },
+                              function (respuesta) {
+                                  $scope.respuesta = respuesta;
+                                  vm.anticipos = sYjResource
+                                      .anticiposByEmpleadoID
+                                      .query({ 'empleadoID': vm.empleadoID });
+                              });
+                        //$rootScope.$broadcast('actualizarTodos', {});
+                    };
+                    $scope.cancel = function () {
+                        $modalInstance.dismiss('cancel');
+                    };
+                }
+                //size: 'sm'
+            });
+            modalInstance.result.then(function (selectedItem) {
+
+            }, function () {
+                //$log.info('Modal dismissed at: ' + new Date());
+                vm.comisiones = sYjResource
+                .anticiposByEmpleadoID.query({ 'empleadoID': vm.empleadoID });
+            });
         }
 
         //Eventos
@@ -19,6 +53,10 @@
             vm.empleadoID = objValRecibido;
             vm.anticipos = sYjResource
                 .anticiposByEmpleadoID.query({ 'empleadoID': vm.empleadoID });
+        });
+        $rootScope.$on('actualizarListadoAnticipos', function (event, objValRecibido) {
+            vm.anticipos = sYjResource
+              .anticiposByEmpleadoID.query({ 'empleadoID': vm.empleadoID });
         });
     }
 })();
