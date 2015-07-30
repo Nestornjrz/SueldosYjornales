837f11635cff8d7f8671df2102ee208b78d1c815
Nestornjrz <nestornjrz@hotmail.com>
2015-07-30 09:42:24 -0400

    Implementacion de guardar el anticipo en el Fron-Ent

--- SueldosYjornales/Scripts/Empleados/Details/Controllers/Anticipos/anticipoCtrl.js
+++ SueldosYjornales/Scripts/Empleados/Details/Controllers/Anticipos/anticipoCtrl.js
@@ -10,6 +10,35 @@
     function anticipoCtrl($rootScope, sYjResource) {
         /* jshint validthis:true */
         var vm = this;
-        
+        vm.anticipo = {};
+        vm.nuevoParaCargar = function () {
+            vm.anticipo = {};
+            vm.anticipo.empleadoID = vm.empleadoID;
+        };
+
+        vm.guardar = function () {
+            sYjResource.anticipos.save(vm.anticipo)
+          .$promise.then(
+              function (mensaje) {
+                  if (!mensaje.error) {
+                      vm.anticipo = mensaje.objetoDto;
+                      vm.mensajeDelServidor = mensaje.mensajeDelProceso;
+                      vm.anticipo.fechaAnticipo = new Date(mensaje.objetoDto.fechaAnticipo);
+                      $rootScope.$broadcast('actualizarListadoAnticipos', {});
+                  } else {
+                      vm.mensajeDelServidor = mensaje.mensajeDelProceso;
+                  }
+              },
+            function (mensaje) {
+                vm.mensajeDelServidor = mensaje.data.mensajeDelProceso;
+            }
+          );
+        }
+
+        //Eventos
+        $rootScope.$on('empleadoID', function (event, objValRecibido) {
+            vm.anticipo.empleadoID = objValRecibido;
+            vm.empleadoID = objValRecibido;
+        });
     }
 })();
