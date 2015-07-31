17d3f0e54ab41c248da8cbf43196a08055ce7c15
Nestornjrz <nestornjrz@hotmail.com>
2015-07-31 16:39:29 -0400

    Preparacion de informes, ahora para liquidacion de salario

---
+++ SueldosYjornales/Scripts/Informes/Index/Controlles/liquidacionSalarioCtrl.js
@@ -0,0 +1,49 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('sueldosYjornalesApp')
+        .controller('liquidacionSalarioCtrl', liquidacionSalarioCtrl);
+
+    liquidacionSalarioCtrl.$inject = ['$rootScope', 'sYjResource'];
+
+    function liquidacionSalarioCtrl($rootScope, sYjResource) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.empresas = sYjResource.empresas.query();
+
+        vm.meses = [
+            { "mesID": 1, "nombreMes": "Enero" },
+            { "mesID": 2, "nombreMes": "Febrero" },
+            { "mesID": 3, "nombreMes": "Marzo" },
+            { "mesID": 4, "nombreMes": "Abril" },
+            { "mesID": 5, "nombreMes": "Mayo" },
+            { "mesID": 6, "nombreMes": "Junio" },
+            { "mesID": 7, "nombreMes": "Julio" },
+            { "mesID": 8, "nombreMes": "Agosto" },
+            { "mesID": 9, "nombreMes": "Setiembre" },
+            { "mesID": 10, "nombreMes": "Octubre" },
+            { "mesID": 11, "nombreMes": "Noviembre" },
+            { "mesID": 12, "nombreMes": "Diciembre" }
+        ];
+
+        var fechaActual = new Date();
+        var yearActual = fechaActual.getFullYear();
+        var mesActual = fechaActual.getMonth();//desde 0 - 11
+
+        vm.years = [];
+        for (var i = -1; i < 5; i++) {
+            vm.years.push(yearActual - i);
+        }
+
+        vm.liquidacionSalario = {};
+        vm.liquidacionSalario.mes = _.findWhere(vm.meses, { "mesID": mesActual + 1  });
+        vm.liquidacionSalario.year = _.find(vm.years, function (num) {
+            return num == yearActual;
+        });
+
+        vm.nuevoParaCargar = function () {
+            vm.liquidacionSalario = {};
+        }
+    }
+})();
---
+++ SueldosYjornales/Scripts/Informes/Index/Services/common.services.js
@@ -0,0 +1,4 @@
+﻿(function () {
+    "use strict";
+    angular.module("common.services", ["ngResource", "ui.bootstrap"])
+}());
