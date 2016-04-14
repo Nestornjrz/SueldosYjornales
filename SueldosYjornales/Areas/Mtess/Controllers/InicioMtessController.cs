809ca57041d8d87728b3be49fdd721e017fd41e3
Nestornjrz <nestornjrz@hotmail.com>
2016-04-14 10:53:48 -0400

    Comienza el proyecto para generar los archivos para el Ministerio de trabajo, empleo y seguridad social

---
+++ SueldosYjornales/Areas/Mtess/Controllers/InicioMtessController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SueldosYjornales.Areas.Mtess.Controllers
+{
+    [Authorize(Roles = "Padrones,Operador,Admin,Supervisor,LiquidadorSalario")]
+    public class InicioMtessController : Controller
+    {
+        // GET: Mtess/InicioMtess
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GenerarArchivos() {
+            return View();
+        }
+    }
+}
---
+++ SueldosYjornales/Areas/Mtess/MtessAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SueldosYjornales.Areas.Mtess
+{
+    public class MtessAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Mtess";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Mtess_default",
+                "Mtess/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
