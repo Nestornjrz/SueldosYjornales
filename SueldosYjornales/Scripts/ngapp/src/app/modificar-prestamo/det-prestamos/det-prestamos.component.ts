8a4db150752924f53554fa6d38b88a2ea0bc5e96
Nestornjrz <nestornjrz@hotmail.com>
2017-06-13 13:34:55 -0400

    Se agrega un @Input en el componente det-prestamos para enviarle el empleadoSelect

--- SueldosYjornales/Scripts/ngapp/src/app/modificar-prestamo/det-prestamos/det-prestamos.component.ts
+++ SueldosYjornales/Scripts/ngapp/src/app/modificar-prestamo/det-prestamos/det-prestamos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { EmpleadoDto } from "app/dtos/empleado-dto";
 
 @Component({
   selector: 'app-det-prestamos',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetPrestamosComponent implements OnInit {
 
+  @Input()   empleadoSelect:EmpleadoDto;;
   constructor() { }
 
   ngOnInit() {
